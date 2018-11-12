f9fa6742cb9c5c5c6cbdb68c71e92f2fa6fb3b91
securitylong <securitylong100@gmail.com>
2018-11-12 12:49:10 +0700

    update

---
+++ A90Check/a90Check/a90Check/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace a90Check
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_status = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(577, 453);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.tabPage1.Controls.Add(this.btn_status);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(569, 427);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Input";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(574, 427);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Checker";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Barcode:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(100, 9);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 28);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btn_status
+            // 
+            this.btn_status.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_status.Location = new System.Drawing.Point(110, 98);
+            this.btn_status.Name = "btn_status";
+            this.btn_status.Size = new System.Drawing.Size(292, 232);
+            this.btn_status.TabIndex = 2;
+            this.btn_status.Text = "Status";
+            this.btn_status.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 456);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button btn_status;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ A90Check/a90Check/a90Check/TfSQL.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Data.OleDb;
+using System.Windows.Forms;
+using Npgsql;
+
+
+namespace a90Check
+{
+    class TfSQL
+    {
+        NpgsqlConnection connection;
+        //  static string conStringIpqcDbP4 = @"Server=192.168.193.4;Port=5432;User Id=pqm;Password=dbuser;Database=ip_pqmdb; CommandTimeout=100; Timeout=100;";
+        static string conStringIpqcDbP4 = @"Server=192.168.145.12;Port=5432;User Id=pqm;Password=dbuser;Database=iot_db; CommandTimeout=100; Timeout=100;";
+
+        public void getComboBoxData(string sql, ref ComboBox cmb)
+        {
+            NpgsqlDataAdapter adapter = new NpgsqlDataAdapter();
+            NpgsqlCommand command;
+            DataSet ds = new DataSet();
+            try
+            {
+                connection = new NpgsqlConnection(conStringIpqcDbP4);
+                connection.Open();
+                command = new NpgsqlCommand(sql, connection);
+                adapter.SelectCommand = command;
+                adapter.Fill(ds);
+                adapter.Dispose();
+                command.Dispose();
+
+                cmb.Items.Clear();
+                foreach (DataRow row in ds.Tables[0].Rows)
+                {
+                    cmb.Items.Add(row[0].ToString());
+                }
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Database Responce", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                connection.Close();
+            }
+        }
+        public void getAutoCompleteData(string sql, ref TextBox txt)
+        {
+            txt.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            txt.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            AutoCompleteStringCollection DataCollection = new AutoCompleteStringCollection();
+
+            NpgsqlDataAdapter adapter = new NpgsqlDataAdapter();
+            NpgsqlCommand command;
+            DataSet ds = new DataSet();
+            try
+            {
+                connection = new NpgsqlConnection(conStringIpqcDbP4);
+                connection.Open();
+                command = new NpgsqlCommand(sql, connection);
+                adapter.SelectCommand = command;
+                adapter.Fill(ds);
+                adapter.Dispose();
+                command.Dispose();
+                connection.Close();
+                foreach (DataRow row in ds.Tables[0].Rows)
+                {
+                    DataCollection.Add(row[0].ToString());
+                }
+                txt.AutoCompleteCustomSource = DataCollection;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Database Responce", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                connection.Close();
+            }
+        }
+        public string sqlExecuteScalarString(string sql)
+        {
+            string response;
+            try
+            {
+                connection = new NpgsqlConnection(conStringIpqcDbP4);
+                connection.Open();
+                NpgsqlCommand command = new NpgsqlCommand(sql, connection);
+                response = Convert.ToString(command.ExecuteScalar());
+                connection.Close();
+                return response;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("SQL executeschalar method failed." + System.Environment.NewLine + ex.Message
+                                , "Database Responce", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                connection.Close();
+                return String.Empty;
+            }
+        }
+        public void sqlDataAdapterFillDatatable(string sql, ref DataTable dt)
+        {
+            NpgsqlConnection connection = new NpgsqlConnection(conStringIpqcDbP4);
+            NpgsqlCommand command = new NpgsqlCommand();
+
+            using (NpgsqlDataAdapter adapter = new NpgsqlDataAdapter())
+            {
+                command.CommandText = sql;
+                command.Connection = connection;
+                adapter.SelectCommand = command;
+                adapter.Fill(dt);
+            }
+        }
+
+    }
+}
