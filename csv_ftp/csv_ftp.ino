ba6938dbfbc7e91dbb7e9c7182f6df4631a49120
securitylong <securitylong100@gmail.com>
2018-11-26 08:02:46 +0700

    udate

--- csv_ftp/csv_ftp.ino
+++ csv_ftp/csv_ftp.ino
@@ -1,10 +1,25 @@
 #include <ESP8266WiFi.h>
 #include <SPI.h>
 #include <String.h>
+#include <SD.h>
 //const char* ssid     = "iPhone";
 //const char* password = "longcoi12345";
 const char* ssid     = "TaoLaGa49";
 const char* password = "longcoi123";
+const String ftp_account = "ftpin";
+const String ftp_passwords = "ftppass";
+const char *server = "192.168.145.7";
+String fileName, path;
+String Folder_name = "459qb";
+const String int_fileName = "arduino.csv";
+const String int_path = "/NC01_";
+WiFiClient client;
+WiFiClient dclient;
+String data1 = "long", data2 = "long";
+char outCount;
+char outBuf[128];
+boolean debug = true;
+boolean upload = true;
 void setup()
 {
   Serial.begin(9600);
@@ -12,7 +27,7 @@ void setup()
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) 
+  while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
     Serial.print(".");
@@ -25,12 +40,182 @@ void setup()
   Serial.println(WiFi.subnetMask());
   Serial.print("Gateway: ");
   Serial.println(WiFi.gatewayIP());
-   digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
   delay(500);
   digitalWrite(LED_BUILTIN, LOW);
   delay(500);
 }
+void efail() {
+  byte thisByte = 0;
+
+  client.println(F("QUIT"));
+
+  while (!client.available()) delay(1);
+
+  while (client.available()) {
+    thisByte = client.read();
+    Serial.write(thisByte);
+  }
+
+  client.stop();
+  Serial.println(F("Command disconnected"));
+}  // efail
+byte eRcv()
+{
+  byte respCode;
+  byte thisByte;
+  while (!client.available()) delay(1);
+  respCode = client.peek();
+  outCount = 0;
+  while (client.available()) {
+    thisByte = client.read();
+    Serial.write(thisByte);
+    if (outCount < 127) {
+      outBuf[outCount] = thisByte;
+      outCount++;
+      outBuf[outCount] = 0;
+    }
+  }
+  if (respCode >= '4') {
+    efail();
+    return 0;
+  }
+  return 1;
+}
+byte doFTP(boolean upload) {
+  if (client.connect(server, 21)) {  // 21 = FTP server
+    Serial.println(F("Command connected"));
+  }
+  else {
+    Serial.println(F("Command connection failed"));
+
+    return 0;
+  }
+  if (!eRcv()) return 0;
+  if (debug) Serial.println("Send USER");
+  client.write("USER ");
+  client.println(ftp_account);
+
+
+  if (!eRcv()) return 0;
+  if (debug) Serial.println("Send PASSWORD");
+  client.write("PASS ");
+  client.println(ftp_passwords);
+
+  if (!eRcv()) return 0;
+  if (debug) Serial.println("Send SYST");
+  //client.println("SYST");
+  client.write("SYST\r\n");
+
+  if (!eRcv()) return 0;
+  if (debug) Serial.println("Send Type I");
+  //client.println("Type I");
+  client.write("Type I\r\n");
+
+
+  if (!eRcv()) return 0;
+  if (debug) Serial.println("Send PASV");
+  //client.println("PASV");
+  client.write("PASV\r\n");
+
+  if (!eRcv()) return 0;
+
+  char *tStr = strtok(outBuf, "(,");
+  int array_pasv[6];
+  for ( int i = 0; i < 6; i++) {
+    tStr = strtok(NULL, "(,");
+    array_pasv[i] = atoi(tStr);
+    if (tStr == NULL) {
+      Serial.println(F("Bad PASV Answer"));
+    }
+  }
+  unsigned int hiPort, loPort;
+  hiPort = array_pasv[4] << 8;
+  loPort = array_pasv[5] & 255;
+
+  if (debug) Serial.print(F("Data port: "));
+  hiPort = hiPort | loPort;
+  if (debug) Serial.println(hiPort);
+
+  if (dclient.connect(server, hiPort)) {
+    Serial.println(F("Data connected"));
+  }
+  else {
+    Serial.println(F("Data connection failed"));
+    client.stop();
+    return 0;
+  }
+
+  if (upload) {
+    if (debug) Serial.println("Send STOR filename");
+    //client.print("MKD ");
+    // client.println(Folder_name);
+    client.print("CWD ");
+    client.println(Folder_name);
+    client.print(F("STOR "));
+    client.println(fileName);
+  } else {
+    if (debug) Serial.println("Send RETR filename");
+    client.print(F("RETR "));
+    client.println(fileName);
+  }
+
+  if (!eRcv()) {
+    dclient.stop();
+    return 0;
+  }
+
+  if (upload) {
+    if (debug) Serial.println(F("Writing to Server"));
+    // for faster upload increase buffer size to 1460
+    //#define bufSizeFTP 64
+#define bufSizeFTP 1460
+    uint8_t clientBuf[bufSizeFTP];
+    //unsigned int clientCount = 0;
+    size_t clientCount = 0;
+
+    dclient.println(data1);
+    //dclient.println(data2);
+    Serial.println(data1);
+    //Serial.println(data2);
+
+  }
+  else {
+    while (dclient.connected()) {
+      while (dclient.available()) {
+        char c = dclient.read();
+        if (debug) Serial.write(c);
+      }
+    }
+  }
+
+  dclient.stop();
+  Serial.println(F("Data disconnected"));
+
+  if (!eRcv()) return 0;
+
+  client.println(F("QUIT"));
+
+  if (!eRcv()) return 0;
+
+  client.stop();
+  Serial.println(F("Command disconnected"));
+
+  //fh.close();
+  if (debug) Serial.println(F("SPIFS closed"));
+  return 1;
+}  // doFTP()
+
 void loop()
 {
-
+  if (doFTP(upload))
+  {
+    Serial.println(F("FTP OK"));
+    Serial.println("");
+  }
+  else
+  {
+    Serial.println(F("NGNGNGNGNGNGNGGNGN"));
+  }
+  delay(3000);
 }
